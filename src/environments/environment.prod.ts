96e5826da9ad124279fab6fe9a52d497b5f2e5cd
yoneyan <github@yoneyan.net>
2021-01-24 04:15:50 +0900

    [change] アドレスの変更

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
     production: true,
     api: {
-        url: 'http://localhost:8081',
+        url: 'http://10.100.1.170:8081',
         path: '/api/v1',
     },
     websocket: {
-        url: 'ws://localhost:8081',
+        url: 'ws://10.100.1.170:8081',
         path: '/ws/v1'
     },
     novnc: {
-        url: 'http://10.100.1.110/noVNC/vnc.html?host=127.0.0.1&port=8081&path=ws/v1/vnc'
+        url: 'http://10.100.1.170/noVNC/vnc.html?host=127.0.0.1&port=8081&path=ws/v1/vnc'
     }
 };
